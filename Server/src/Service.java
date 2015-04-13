a8f3f47cc4e42b396b08a22ec0e4e6a607a79d7c
Paolo Ascurra <paolo@ascurra.com>
2015-04-13 16:33:00 -0400

    Fix communication problem. Tested with delete client option.
    Please, test your stories and modify using as a model the "delete client" option

--- Server/src/Service.java
+++ Server/src/Service.java
@@ -70,11 +70,25 @@ public class Service {
 				System.out.println("Server has received \"" + line
 						+ "\" from the client");
 				// Reverse the message and send it back to the client,
-				out.println(new StringBuilder(line).toString());
+				
+				
+				if (line.contains("<"))
+				{
 				xml = line;
+				break;
+				}
+				
+				
+				
 				if (line.equals("exit"))
 					break;
+				
+
+				
 			}
+			
+			//process sth
+
 
 			Document doc = null;
 
@@ -132,6 +146,11 @@ public class Service {
 				// databaseConnection registerQuery=new
 				// databaseConnection("INSERT INTO...");
 				// ResultSet result=registerQuery.Query();
+				
+				//reply to waiting client
+
+				//FIX: Reply to waiting client, by Paolo
+				out.println(new StringBuilder("sucess").toString());
 
 			}
 
@@ -149,6 +168,13 @@ public class Service {
 				// query db to delete
 				// deregisterQuery.query = "";
 				deregisterQuery.ExecuteUpdate();
+				
+
+
+				
+				//FIX: Reply to waiting client, by Paolo
+				out.println(new StringBuilder("sucess").toString());
+				
 
 			}
 
@@ -190,6 +216,10 @@ public class Service {
 				subscribeQuery.query = "INSERT INTO ascurra_445.subscribers(client_alias,following_client_id) VALUES ('"
 						+ alias + "', '" + subscribeToId + "' )";
 				subscribeQuery.ExecuteUpdate();
+				
+				
+				//FIX: Reply to waiting client, by Paolo
+				out.println(new StringBuilder("sucess").toString());
 
 			}
 
@@ -341,12 +371,24 @@ public class Service {
 				} catch (MessagingException mex) {
 					mex.printStackTrace();
 				}
+				
+				
+				//FIX: Reply to waiting client, by Paolo
+				out.println(new StringBuilder("sucess").toString());
+				
 			}
 
 			// End of Creating Twibble
 
 			// Updating Profile
 			if (command.equals("Update Profile")) {
+				
+				
+				
+				
+				
+				//FIX: Reply to waiting client, by Paolo
+				out.println(new StringBuilder("sucess").toString());
 
 			}
 			// End Of Updating Profile
@@ -429,6 +471,10 @@ public class Service {
 
 				// Ending message
 				System.out.println("New user registered...........");
+				
+				
+				//FIX: Reply to waiting client, by Paolo
+				out.println(new StringBuilder("sucess").toString());
 
 			}
 
@@ -451,6 +497,10 @@ public class Service {
 				deleteTwibbleQuery.ExecuteUpdate();
 				
 				System.out.println("Twibble deleted!");
+				
+				
+				//FIX: Reply to waiting client, by Paolo
+				out.println(new StringBuilder("sucess").toString());
 
 			}
 			
@@ -490,6 +540,10 @@ public class Service {
 				databaseConnection deleteProfileQuery = new databaseConnection("");
 				deleteProfileQuery.query = "DELETE FROM ascurra_445.profiles WHERE idForeignKey= '" + fKeyId + "' ";
 				deleteProfileQuery.ExecuteUpdate();
+				
+
+				//FIX: Reply to waiting client, by Paolo
+				out.println(new StringBuilder("sucess").toString());
 				
 				
 			}
