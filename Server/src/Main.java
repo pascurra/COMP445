a94bb36418e036b171eb24ffb991b3431360309f
Paolo Ascurra <paolo@ascurra.com>
2015-04-15 23:10:25 -0400

    Multithreading Implementation.
    See http://www.javatpoint.com/synchronization-in-java

--- Server/src/Main.java
+++ Server/src/Main.java
@@ -2,18 +2,109 @@ import java.io.*;
 import java.net.*;
 import java.sql.SQLException;
 
-public class Main 
-{
-	public static void main(String[] args) throws SQLException 
-	{
+public class Main {
+	public static void main(String[] args) throws SQLException {
+
+		final Service service1 = new Service(1);
+
+		while (service1.terminate != true) {
 			
-		Service service= new Service();
-		while (service.terminate!=true){
-				service.Execute();
-				
-				
+			Thread t1 = new Thread() {
+				public void run() {
+					try {
+						System.out.println("Starting Thread 1");
+						service1.Execute();
+						System.out.println("Ending Thread 1");
+
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			};
+
+			Thread t2 = new Thread() {
+				public void run() {
+					try {
+						System.out.println("Starting Thread 2");
+
+						service1.Execute();
+						System.out.println("Ending Thread 2");
+
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			};
+
+			Thread t3 = new Thread() {
+				public void run() {
+					try {
+						System.out.println("Starting Thread 3");
+
+						service1.Execute();
+						System.out.println("Ending Thread 3");
+
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			};
+
+			Thread t4 = new Thread() {
+				public void run() {
+					try {
+						System.out.println("Starting Thread 4");
+
+						service1.Execute();
+						System.out.println("Ending Thread 4");
+
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			};
+
+			Thread t5 = new Thread() {
+				public void run() {
+					try {
+						System.out.println("Starting Thread 5");
+
+						service1.Execute();
+						
+						System.out.println("Ending Thread 5");
+
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			};
+						
+			t1.start();
+			t2.start();
+			t3.start();
+			t4.start();
+			t5.start();			
+			
+			try {
+				t1.join();
+				t2.join();
+				t3.join();
+				t4.join();
+				t5.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			
+			
 		}
-		
+
 	}
 
 }
--- Server/src/Service.java
+++ Server/src/Service.java
@@ -30,26 +30,48 @@ import org.xml.sax.InputSource;
 
 public class Service {
 
-	public Service() {
+	public Service(int threadId) {
 		super();
+		this.threadId=threadId;
+		
 	}
-
+	int threadId=0;
 	Boolean terminate = false;
 
 	ServerSocket server = null;
 	Socket socket; // Client Socket
-	int serverPort = 6789;
+	int serverPort;
 
 	PrintWriter out;
 	BufferedReader in;
-	Scanner input = new Scanner(System.in);
+	Scanner input;
+	
+	
+	
+    public void run() {
+    	
+    	try {
 
-	public void Execute() throws SQLException {
+			this.Execute();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    }
+	
+	
+
+	public synchronized void Execute() throws SQLException {
+		serverPort = 6789;
+		
+		input = new Scanner(System.in);		
 
 		try {
 			// Create a Socket and bind it to a port
 			server = new ServerSocket(serverPort);
-			System.out.println("Server is up and running...");
+			System.out.println("Server "+this.threadId +"is up and running...");
 
 			// Accept a connection from the client and associate a Socket to
 			// this connection
