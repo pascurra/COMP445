5cd256ee44dfd8d758aec087ade13ea148c66e14
Paolo Ascurra <paolo@ascurra.com>
2015-04-16 01:27:29 -0400

    fixed bug

--- Server/src/Service.java
+++ Server/src/Service.java
@@ -329,6 +329,22 @@ public class Service {
 					}
 				}
 				
+				
+				
+				
+				//Insert twibble to database
+				
+				
+				databaseConnection createTwibbleQuery = new databaseConnection(
+						"");
+				createTwibbleQuery.query = "INSERT INTO ascurra_445.twibbles(twiblrcontent,usersIdForeign) VALUES ('"
+						+ twibbleContent + "','" + userId + "')";
+				createTwibbleQuery.ExecuteUpdate();
+				System.out.println("New Twibble Posted: " + twibbleContent);
+				
+				
+				
+				
 				if(subscriberEmails.size()>0){
 
 				InternetAddress[] cc = new InternetAddress[subscriberEmails
@@ -338,12 +354,7 @@ public class Service {
 				}
 				System.out.println(cc);
 					
-				databaseConnection createTwibbleQuery = new databaseConnection(
-						"");
-				createTwibbleQuery.query = "INSERT INTO ascurra_445.twibbles(twiblrcontent,usersIdForeign) VALUES ('"
-						+ twibbleContent + "','" + userId + "')";
-				createTwibbleQuery.ExecuteUpdate();
-				System.out.println("New Twibble Posted: " + twibbleContent);
+				
 
 				// Send Notification
 				// Recipient's email ID needs to be mentioned.
